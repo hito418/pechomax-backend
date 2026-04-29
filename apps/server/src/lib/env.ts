69c9640ddea3865c67828fcd52043f4af9c26b95
hito418 <hito.wt@gmail.com>
2026-04-29 10:32:39 +0200

    feat: add env error

--- apps/server/src/lib/env.ts
+++ apps/server/src/lib/env.ts
@@ -13,14 +13,15 @@ const envSchema = type({
   FIREBASE_CLIENT_EMAIL: 'string',
   FIREBASE_PRIVATE_KEY: 'string',
   FIREBASE_STORAGE_BUCKET: 'string',
-  PAGE_SIZE: type("string.numeric").default('15'),
-  MAX_FILE_SIZE: type("string.numeric").default((10 * 1024 * 1024).toString()),
-  APP_PORT: type("string.numeric").default("3000"),
+  PAGE_SIZE: type('string.numeric').default('15'),
+  MAX_FILE_SIZE: type('string.numeric').default((10 * 1024 * 1024).toString()),
+  APP_PORT: type('string.numeric').default('3000'),
   NODE_ENV: '"DEV" | "PROD"',
 })
 
 const result = envSchema(process.env)
 if (result instanceof type.errors) {
+  console.error(result)
   console.error('Invalid Environment Variables')
   process.exit(1)
 }
